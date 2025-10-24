79d660c5144a30fa8928b5ab061a572ac4dbba51
neyzapcr <neyza24si@mahasiswa.pcr.ac.id>
2025-10-24 08:39:02 +0700

    edit di sidebar.blade di line 60 dan 73

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -57,7 +57,7 @@
                     <span class="mt-1 ms-1 sidebar-text">Volt Overview</span>
                 </a>
             </li>
-            <li class="nav-item  active ">
+            <li class="nav-item  {{ request()->routeIs('dashboard') ? 'active' : '' }}">
                 <a href="{{ route('dashboard') }}" class="nav-link">
                     <span class="sidebar-icon">
                         <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20"
@@ -70,8 +70,8 @@
                 </a>
             </li>
             <!-- Tambahkan blok kode ini -->
-            <li class="nav-item  active ">
-                <a href="{{ route('pelanggan.create') }}" class="nav-link">
+            <li class="nav-item {{ request()->routeIs('pelanggan.*') ? 'active' : '' }}">
+                <a href="{{ route('pelanggan.index') }}" class="nav-link">
                     <span class="sidebar-icon">
                         <svg class="icon icon-xs me-2" data-slot="icon" fill="none" stroke-width="1.5"
                             stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
