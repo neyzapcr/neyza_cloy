50244d21e5338663920886d709ecf2f08cfa5cdb
neyzapcr <neyza24si@mahasiswa.pcr.ac.id>
2025-10-24 08:14:50 +0700

    tambah partial view

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -57,7 +57,7 @@ class PelangganController extends Controller
     public function edit(string $id)
     {
         $data['dataPelanggan'] = Pelanggan::findOrFail($id);
-        return view('admin.pelanggan.edit', $data);
+        return view('admin.pelanggan.index', $data);
     }
 
     /**
